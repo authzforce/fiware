1c43a8bac1718b2d866a2dec605d334e85060981
cdanger <cdanger@users.noreply.github.com>
2021-08-04 20:53:55 +0200

    Updated authzforce server version: 10.1.1

--- doc/conf.py
+++ doc/conf.py
@@ -93,9 +93,9 @@ author = u'Cyril Dangerville, THALES'
 # built documents.
 #
 # The short X.Y version.
-version = '10.1.0'
+version = '10.1.1'
 # The full version, including alpha/beta/rc tags.
-release = '10.1.0'
+release = '10.1.1'
 
 rst_epilog = """
     .. |product.version| replace:: {0}
