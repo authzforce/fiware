c07cc1f345475a3700abcf985d4659878e7ac9fa
Cyril Dangerville <1372580+cdanger@users.noreply.github.com>
2024-02-05 04:00:25 +0100

    Fixed doc/conf.py for readthedocs.org

--- doc/conf.py
+++ doc/conf.py
@@ -66,6 +66,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
